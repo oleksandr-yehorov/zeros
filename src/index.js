e7f8025c905294e69be41851cb2cd0408930aa02
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-10-12 00:34:49 +0300

    feat: init factorial arrays

--- src/index.js
+++ src/index.js
@@ -1,32 +1,15 @@
 module.exports = function zeros(expression) {
   // your solution
-  var fact = expression.split('*'); // split expression by '*'
-  console.log(fact);
+  
+  // Split expression into array
+  let arrayFactorial = expression.split('*'); // split expression by '*'
+  
+  // Init factorial arrays
+  let factorialArray = [];
+  let doubleFactorialArray = [];
 
-  var factorial = [];
-  for (var i=0; i<fact.length;i++) {
-    factorial[i] = parseInt(fact[i], 10);
-  }
+  
 
-  console.log(factorial);
-  var zero = 0;
-
-  for (var i=0; i<factorial.length; i++) {
-
-    if (/!!/.test(fact[i]) === true) {
-      console.log('!');
-      console.log(fact[i]);
-      zero=zero + Math.ceil(factorial[i]/10);
-
-    } else if (/!!/.test(fact[i]) === true) {
-      console.log('!!');
-      console.log(factorial[i]);
-      zero=zero + Math.ceil(factorial[i]/5);
-    }
-
-  };
-  console.log(zero);
-
-  return zero;
+  return zeros;
 
 }
