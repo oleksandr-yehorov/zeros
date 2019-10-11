0f0001600fde32cbbed0189433aaf7227e66ac1e
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-10-12 00:35:43 +0300

    feat: dividing arrays with factorials

--- src/index.js
+++ src/index.js
@@ -7,9 +7,18 @@ module.exports = function zeros(expression) {
   // Init factorial arrays
   let factorialArray = [];
   let doubleFactorialArray = [];
-
   
-
+  //Dividing arrays with factorials
+  for (let i = 0; i < arrayFactorial.length; i++) {
+    if (arrayFactorial[i].includes('!!')) {
+      let double = parseFloat(MasWithFact[i]);
+      doubleFactorialArray.push(double);
+    } else {
+      let single = parseFloat(MasWithFact[i]);
+      factorialArray.push(single);
+    }
+  };
+  
   return zeros;
 
 }
