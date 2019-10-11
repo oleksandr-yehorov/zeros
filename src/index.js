e9227e0fac4d9cc9bd0b9af85a052ca398c16058
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-10-11 21:40:33 +0300

    feat: counting zeros in expression

--- src/index.js
+++ src/index.js
@@ -7,6 +7,25 @@ module.exports = function zeros(expression) {
   for (var i=0; i<fact.length;i++) {
     factorial[i] = parseInt(fact[i], 10);
   }
+
+  console.log(factorial);
+  var zero = 0;
+
+  for (var i=0; i<factorial.length; i++) {
+
+    if (/!!/.test(fact[i]) === true) {
+      console.log('!');
+      console.log(fact[i]);
+      zero=zero + Math.floor(factorial[i]/10);
+
+    } else if (/!!/.test(fact[i]) === true) {
+      console.log('!!');
+      console.log(factorial[i]);
+      zero=zero + Math.floor(factorial[i]/5);
+    }
+
+  };
+  console.log(zero);
   
   return zero;
 
