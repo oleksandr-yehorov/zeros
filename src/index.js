e60e7854419d071094eca2d58bf702a1b0850619
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-10-12 22:39:02 +0300

    feat: split expression into array

--- src/index.js
+++ src/index.js
@@ -4,21 +4,7 @@ module.exports = function zeros(expression) {
   // Split expression into array
   let arrayFactorial = expression.split('*'); // split expression by '*'
   
-  // Init factorial arrays
-  let factorialArray = [];
-  let doubleFactorialArray = [];
-  
-  //Dividing arrays with factorials
-  for (let i = 0; i < arrayFactorial.length; i++) {
-    if (arrayFactorial[i].includes('!!')) {
-      let double = parseFloat(MasWithFact[i]);
-      doubleFactorialArray.push(double);
-    } else {
-      let single = parseFloat(MasWithFact[i]);
-      factorialArray.push(single);
-    }
-  };
-  
+
   return zeros;
 
 }
